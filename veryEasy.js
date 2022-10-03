0b432bf99ef006552a1167c4c47724d393b8be08
Carolina Urrea <curestrepo@gmail.com>
2022-10-03 16:20:26 -0700

    5 challenges day 1

---
+++ veryEasy.js
@@ -0,0 +1,28 @@
+console.log('sum of two numbers')
+function addition(a, b) {
+	let sum = a + b
+	return sum
+}
+
+console.log('turn seconds to hours')
+function howManySeconds(hours) {
+	let seconds = 60 * hours
+	let hour = seconds * 60
+	return hour
+}
+
+function addition(num) {
+	let plus = num + 1
+	return plus
+}
+
+function findPerimeter(length, width) {
+	let y = width * 2
+	let x = length *2
+	return x + y	
+}
+
+function remainder(x, y) {
+	let rem = x % y
+	return rem
+}
