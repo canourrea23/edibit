c782f596501842b83cdb45b9d617e57f18bcc182
Carolina Urrea <curestrepo@gmail.com>
2022-10-04 17:57:30 -0700

    day 2

--- veryEasy.js
+++ veryEasy.js
@@ -26,3 +26,36 @@ function remainder(x, y) {
 	let rem = x % y
 	return rem
 }
+
+console.log('power')
+function circuitPower(voltage, current) {
+	let power = voltage * current
+	return power
+}
+
+console.log('area of triangle')
+function triArea(base, height) {
+	let area = base * height / 2
+	return area
+}
+
+console.log('true if 0 or less')
+function lessThanOrEqualToZero(num) {
+	if (num <= 0){
+		return true
+	} else {
+		return false
+	}
+}
+
+console.log('add the point')
+function points(twoPointers, threePointers) {
+	let twoPoints = twoPointers * 2
+	let threePoints = threePointers * 3
+	return twoPoints + threePoints
+}
+
+console.log('Return the First Element in an Array')
+function getFirstValue(arr) {
+	return arr[0]
+}
